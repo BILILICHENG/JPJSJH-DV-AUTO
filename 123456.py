bcfa5cc681c33e1a81f06d9001a35fad0d96b9ab
LI_CHENG <90380376+BILILICHENG@users.noreply.github.com>
2024-12-03 05:24:53 +0900

    Update 123456.py

--- 123456.py
+++ 123456.py
@@ -42,35 +42,35 @@ def fetch_earthquake_data():
 
             formatted_data = [f"{row[0]}={row[1]}={row[2]}={row[3]}={row[4]}" for row in data]
 
-            # 从GitHub raw链接获取存储的地震数据
-            url = "https://raw.githubusercontent.com/BILILICHENG/JPJSJH-DV-AUTO/refs/heads/main/earthquake_data.txt"  # 替换为实际的raw文件链接
+            
+            url = "https://raw.githubusercontent.com/BILILICHENG/JPJSJH-DV-AUTO/refs/heads/main/earthquake_data.txt"  
             response = requests.get(url)
             saved_data = response.text.splitlines()
 
-            # 比对气象厅获取到的数据和GitHub存储的数据
+            
             new_data = []
             for line in formatted_data:
                 if line not in saved_data:
                     new_data.append(line)
 
-            # 如果有新数据
+            
             if new_data:
-                # 保存新数据到latest_earthquake_data.txt
+                
                 with open('latest_earthquake_data.txt', 'w', encoding='utf-8') as f:
                     f.writelines([line + "\n" for line in new_data])
 
-                # 更新完整数据文件
+                
                 with open('earthquake_data.txt', 'w', encoding='utf-8') as f:
                     f.writelines([line + "\n" for line in formatted_data])
 
-                # 生成Discord消息
+                
                 message = ""
                 for line in new_data:
                     parts = line.strip().split('=')
                     formatted_message = f"震央地名：{parts[1]}\nマグニチュード：{parts[2]}\n最大震度：{parts[3]}\n地震検知日時：{parts[0]}\n気象庁発表日時：{parts[4]}"
                     message += formatted_message + "\n------------\n"
                 
-                # 发送Discord通知
+                
                 payload = {"content": message}
                 response = requests.post(DISCORD_WEBHOOK_URL, json=payload)
 
@@ -82,7 +82,7 @@ def fetch_earthquake_data():
 
             else:
                 print("没有新的数据。")
-                # 即使没有新数据，还是保存气象厅的最新数据
+                
                 with open('earthquake_data.txt', 'w', encoding='utf-8') as f:
                     f.writelines([line + "\n" for line in formatted_data])
 
